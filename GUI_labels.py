aaa33a8c4c8cacba19fe2b43bc0ee5bbecf1a101
Shridhar Bhat <bhatshridhar27@gmail.com>
2022-10-10 20:31:15 +0530

    Python GUI labels 10-Oct-2022

---
+++ GUI_labels.py
@@ -0,0 +1,27 @@
+from tkinter import*
+
+# label-->an area widget that holds text/image within a window..
+# fg-->foreground
+# relief--> border around label
+# bd--> border width
+# padding --> padx & pady
+# compound--> where we want to place the image
+
+window=Tk()
+photo=PhotoImage(file="logo.png")
+label=Label(window,
+            text="Welcome to Bangalore",
+            font=("Arial",40,"bold"),
+            fg="green",
+            bg="black",
+            relief=RAISED,
+            bd=10,
+            padx=20,
+            pady=20,
+            image=photo,
+            compound="bottom") # image appears at the bottom
+label.pack()
+label.place(x=100,y=300) # where you can place text or images...
+
+
+window.mainloop()
